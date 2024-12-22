a2b2a71c82b756ce268d3a263e0922c9f5599f35
thequang <nguyenthequangdev@gmail.com>
2024-12-22 22:39:51 +0700

    16.[CRUD] Delete coure (Xây dựng chức năng xóa khóa học)

--- src/routes/courses.js
+++ src/routes/courses.js
@@ -13,6 +13,8 @@ router.get('/:id/edit', courseController.edit);
 
 router.put('/:id', courseController.update);
 
+router.delete('/:id', courseController.destroy);
+
 router.get('/:slug', courseController.show);
 
 module.exports = router;
